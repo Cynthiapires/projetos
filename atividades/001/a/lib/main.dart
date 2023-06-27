a703587da8ef34836c3c71500deab69daadc6dd2
Cynthiapires <cynthia.isabelle.pires@gmail.com>
2023-06-27 19:18:13 -0300

    27/06

--- atividades/001/a/lib/main.dart
+++ atividades/001/a/lib/main.dart
@@ -1,115 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: mediaAritmetica(),
+));
+class mediaAritmetica extends StatefulWidget {
+  const mediaAritmetica({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<mediaAritmetica> createState() => _mediaAritmeticaState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _mediaAritmeticaState extends State<mediaAritmetica> {
+  final TextEditingController _textEditingController_1 = TextEditingController();
+  final TextEditingController _textEditingController_2 = TextEditingController();
 
-  void _incrementCounter() {
+  double _valor1 = 0;
+  double _valor2 = 0;
+  String _resultado = "Resultado: ";
+
+  void recuperarValor() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _valor1 = double.parse(_textEditingController_1.text);
+      _valor2 = double.parse(_textEditingController_2.text);
+
+      _resultado = "Resultado: " + ((_valor1 + _valor2) / 2).toString();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("App Média"),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+      body: Column(
+        children: [
+          Image.asset("imagem/logo.png"),
+
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
+            child: Text(
+              "Preencha os campos abaixo",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blue
+              ),
+          )),
+
+          Padding(
+            padding: EdgeInsets.all(20),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+
+              children: [
+                
+                //Campo 1
+                TextField(
+                  keyboardType: TextInputType.number,
+
+                  decoration: InputDecoration(
+                    labelText: "Informe o primeiro valor"
+                  ),
+
+                  enabled: true,
+
+                  controller: _textEditingController_1,
+                ),
+
+                //Campo 2
+                TextField(
+                  keyboardType: TextInputType.number,
+
+                  decoration: InputDecoration(
+                    labelText: "Informe o segundo valor"
+                  ),
+
+                  enabled: true,
+
+                  controller: _textEditingController_2,
+                ),
+
+                //Calcular
+                Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: ElevatedButton(
+                    onPressed: recuperarValor, 
+                    
+                    child: Text("Calcular"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber
+                    )
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      _resultado,
+                      
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 19,
+                        color: Colors.redAccent
+                      )
+                    ))
+                ),
+              )
+            ],
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
